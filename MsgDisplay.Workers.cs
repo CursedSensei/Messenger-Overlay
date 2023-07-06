0025af9de28d38e069ab70814a0f69216ff4cd1b
Zillion <Zillion@Sora>
2023-07-07 02:30:40 +0800

    Code cleanup

---
+++ MsgDisplay.Workers.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Messenger_Overlay
+{
+    partial class MsgDisplay
+    {
+        private void sKeyWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            bool kBuffer = false;
+            while (true)
+            {
+                this.Invoke((MethodInvoker)delegate ()
+                {
+                    if (Keyboard.IsKeyDown(Key.LeftShift) &&
+                        Keyboard.IsKeyDown(Key.F3))
+                    {
+                        if (kBuffer != true)
+                        {
+                            kBuffer = true;
+                            ShowDisplay();
+                        }
+                    }
+                    else { kBuffer = false; }
+                });
+
+                System.Threading.Thread.Sleep(30);
+            }
+        }
+
+        private void delayedInit_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (!sKeyWorker.IsBusy) { }
+            System.Threading.Thread.Sleep(50);
+            this.ShowDisplay();
+        }
+    }
+}
--- MsgDisplay.cs
+++ MsgDisplay.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows.Forms;
-using System.Windows.Input;
-using MouseEventHandler = System.Windows.Forms.MouseEventHandler;
 
 namespace Messenger_Overlay
 {
@@ -34,35 +32,6 @@ namespace Messenger_Overlay
             {
                 this.Hide();
             }
-        }
-        private void sKeyWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
-        {
-            bool kBuffer = false;
-            while (true)
-            {
-                this.Invoke((MethodInvoker)delegate ()
-                {
-                    if (Keyboard.IsKeyDown(Key.LeftShift) &&
-                        Keyboard.IsKeyDown(Key.F3))
-                    {
-                        if (kBuffer != true)
-                        {
-                            kBuffer = true;
-                            ShowDisplay();
-                        }
-                    }
-                    else { kBuffer = false; }
-                });
-                
-                System.Threading.Thread.Sleep(30);
-            }
-        }
-
-        private void delayedInit_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
-        {
-            while(!sKeyWorker.IsBusy) { }
-            System.Threading.Thread.Sleep(50);
-            this.ShowDisplay();
         }
     }
 }
